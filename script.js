ae3c184ef562e6d077b790907a0624e39b4f2ba8
felianfilian <darianbellentor@outlook.de>
2023-05-11 22:26:38 +0200

    first logic

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function loadPokedex() {
-  // code
+function loadPokemon() {
+  loadPokedex();
+}
+
+async function loadPokedex() {
+  let url = "https://pokeapi.co/api/v2/pokemon/charmander";
+  let response = await fetch(url);
+  let responseJson = response.json();
+  console.log(responseJson);
 }
