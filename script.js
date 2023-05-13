6ae35e5823b6db3ae8763bb03130fb899ccb0921
felianfilian <darianbellentor@outlook.de>
2023-05-13 23:30:58 +0200

    pokedex main file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let currentPokemon;
 
 function loadPokemon() {
   loadPokedex();
+  changeStatTab(0);
 }
 
 async function loadPokedex() {
@@ -23,4 +24,25 @@ function renderPokemonInfo() {
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function changeStatTab(index) {
+  let content = "Page not found";
+  if (index == 0) {
+    content = `
+    <ul class="stat-list">
+      <li class="stat-element">Element: Fire</li>
+      <li class="stat-element">Species: Dragon</li>
+    </ul>
+    `;
+  }
+  if (index == 1) {
+    content = `
+    <ul class="stat-list">
+      <li class="stat-element">Strength: 12</li>
+      <li class="stat-element">health: 60</li>
+    </ul>
+    `;
+  }
+  document.getElementById("stats-content").innerHTML = content;
 }
